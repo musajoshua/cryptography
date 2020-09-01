b624dffa57203ea6013db6c72a9f1711a0d76ad5
Musa Joshua <gidijosh@gmail.com>
2020-09-01 20:41:09 +0100

    add rsa eel expose function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import io
 from mod.modules.RSA import RSA
+from mod.modules.ELGamal import ELGamal
 from mod.modules.FileReader import FileReader
 from base64 import b64encode
 import eel
@@ -21,7 +22,10 @@ def rsa_encrypt(public_key, plainString):
     n = public_key[0]
     e = public_key[1]
     public_key = (n, e)
-    cipherString, time_taken_encrypt = RSA.encrypt(plainString, public_key)
+    cipherString, time_taken_encrypt = RSA.encrypt(
+        plainString,
+        public_key
+    )
     return (cipherString, time_taken_encrypt)
 
 
@@ -31,7 +35,45 @@ def rsa_decrypt(private_key, cipherString):
     n = private_key[0]
     d = private_key[1]
     private_key = (n, d)
-    plainString, time_taken_decrypt = RSA.decrypt(cipherString, private_key)
+    plainString, time_taken_decrypt = RSA.decrypt(
+        cipherString,
+        private_key
+    )
+    return (plainString, time_taken_decrypt)
+
+
+# ELGamal KEY GENERATION
+@eel.expose
+def elgamal_key_gen():
+    pubkey, privkey = ELGamal.generate_key()
+    return (pubkey, privkey)
+
+
+# ELGamal ENCRYPTION
+@eel.expose
+def elgamal_encrypt(public_key, plainString):
+    p = public_key[0]
+    g = public_key[1]
+    y = public_key[2]
+    public_key = (p, g, y)
+    (c1, en_msg), time_taken_encrypt = ELGamal.encrypt(
+        plainString,
+        public_key
+    )
+    return ((c1, en_msg), time_taken_encrypt)
+
+
+# ELGamal DECRYPTION
+@eel.expose
+def elgamal_decrypt(private_key, c1, en_msg):
+    p = private_key[0]
+    x = private_key[1]
+    private_key = (p, x)
+    plainString, time_taken_decrypt = ELGamal.decrypt(
+        private_key,
+        c1,
+        en_msg
+    )
     return (plainString, time_taken_decrypt)
 
 
